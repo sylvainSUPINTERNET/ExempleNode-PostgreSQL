fb8a835cdfb6237e904200df79c11392c6b9633f
JOLY Sylvain <jolysylvain@MPEC-PDT-33.local>
2018-07-09 17:43:59 +0200

    other test

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.get('/testQuery', function (req, res) {
 
 app.get('/department/:id', function(req,res){
    const deptId = req.params.id;
-   
+
    //override query method to be async query
-   async function perfomQueryAsync(){
+   async function performQueryAsync(){
        try {
            let result = await psql.Client.query("SELECT * from department WHERE id = $1", [deptId]);
             return result.rows;
@@ -40,13 +40,35 @@ app.get('/department/:id', function(req,res){
        }
    }
 
-   perfomQueryAsync()
+   performQueryAsync()
        .then(function(rows){
            res.status(200).json({error:false, data:rows})
        })
        .catch(function(err){
            res.status(400).json({error:true, data:err})
        })
+});
+
+
+app.get('/department/get/list', function(req,res){
+
+    //override query method to be async query
+    async function performQueryAsync(){
+        try {
+            let result = await psql.Client.query("SELECT * FROM department");
+            return result.rows;
+        } catch(e){
+            return e;
+        }
+    }
+
+    performQueryAsync()
+        .then(function(rows){
+            res.status(200).json({error:false, data:rows})
+        })
+        .catch(function(err){
+            res.status(400).json({error:true, data:err})
+        })
 });
 
 
